451a4e30fdc286ae7842dda9777bcd6a2594c063
ppbao <bruce.liu.02@gmail.com>
2016-12-31 19:21:32 +1000

    Initial commit

---
+++ app/src/main/java/au/com/unisharing/newtest/MainActivity.java
@@ -0,0 +1,13 @@
+package au.com.unisharing.newtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
